1fda2b36ab557fecd20103e3690a89407336f101
Vadim-Vatrya <VatryaVV@gmail.com>
2021-12-21 15:51:37 +0200

    add api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { ToastContainer } from 'react-toastify';
 
+import Container from './components/Container';
+// import Weatherboard for './components/Weatherboard';
+
 import './scss/_main.scss';
 
 function App() {
   return (
     <>
-      {/* <Container> */}
-      {/* <WeatherDisplay /> */}
-      <ToastContainer autoClose={3700} position="bottom-center" />
-      {/* </Container> */}
+      <Container>
+        {/* <Weatherboard /> */}
+        <ToastContainer autoClose={3700} position="bottom-center" />
+      </Container>
     </>
   );
 }
