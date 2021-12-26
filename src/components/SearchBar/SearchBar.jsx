7ef212473fe0863f702871b2f54c4031851c0534
Vadim-Vatrya <VatryaVV@gmail.com>
2021-12-27 00:19:16 +0200

    end searchbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -6,6 +6,38 @@ import styles from './SearchBar.module.scss';
 
 const SearchBar = ({ onHadleSubmit }) => {
   const [query, setQuery] = useState('');
+
+  const onSubmit = event => {
+    event.preventDefault();
+
+    if (query.trim === '') {
+      return toast.info('😱 Please enter a value for search city!');
+    }
+
+    onHadleSubmit(query);
+    setQuery('');
+  };
+
+  return (
+    <>
+      <header className={styles.input}>
+        <form className={styles.form} onSubmit={onSubmit}>
+          <input
+            className={styles.input}
+            autoComplete="off"
+            autoFocus
+            value={query}
+            type="text"
+            placeholder="Search city..."
+            onChange={({ target }) => setQuery(target.value)}
+          />
+        </form>
+        <button type="submit" className={styles.btn}>
+          <span className={styles.label}>Search</span>
+        </button>
+      </header>
+    </>
+  );
 };
 
 SearchBar.propTypes = {
