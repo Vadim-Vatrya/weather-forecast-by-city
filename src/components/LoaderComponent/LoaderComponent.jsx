e921473684290c37ba3630313c8eb6aa27e11be9
Vadim-Vatrya <VatryaVV@gmail.com>
2021-12-29 23:35:13 +0200

    loader

---
+++ src/components/LoaderComponent/LoaderComponent.jsx
@@ -0,0 +1,22 @@
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import Loader from 'react-loader-spinner';
+
+import styles from 'LoaderComponent.module.scss';
+
+const LoaderComponent = () => {
+  return (
+    <>
+      <div className={styles.LoaderSpinner}>
+        <Loader
+          type="Grid"
+          color="#00BFFF"
+          height={80}
+          width={80}
+          timeout={3000}
+        />
+      </div>
+    </>
+  );
+};
+
+export default LoaderComponent;
