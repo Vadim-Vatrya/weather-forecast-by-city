fe2a449a490c6d981ea601ff119c635df7e47fa5
Vadim-Vatrya <VatryaVV@gmail.com>
2021-12-30 00:58:35 +0200

    dataview

---
+++ src/components/DataTime/DateTime.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect, useRef } from 'react';
+import { Month, Weekday, Day } from '../../helpers/utils';
+import clock from '../../images/clock.png';
+import styles from './DateTime.module.css';
+
+function Clock() {
+  const [time, setTime] = useState(() => new Date().toLocaleTimeString());
+  const intervalId = useRef(null);
+
+  useEffect(() => {
+    intervalId.current = setInterval(() => tick(), 1000);
+
+    return () => {
+      stop();
+    };
+  }, []);
+
+  const tick = () => {
+    setTime(new Date().toLocaleTimeString('en-US', { timeStyle: 'short' }));
+  };
+
+  const stop = () => {
+    clearInterval(intervalId.current);
+  };
+
+  return (
+    <div className={styles.container}>
+      <img
+        className={styles.icon}
+        src={clock}
+        alt="time icon"
+        width="38"
+        height="38"
+      />
+      <div className={styles.wrapper}>
+        <p className={styles.time}>{time}</p>
+        <p className={styles.date}>
+          {Weekday}, {Month} {Day}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Clock;
---
+++ src/components/DataView/DataView.jsx
@@ -0,0 +1,92 @@
+import DateTime from '../DataTime';
+
+import temperature from '../../images/temperature.png';
+import wind from '../../images/wind.png';
+import himidity from '../../images/humidity.png';
+
+import styles from 'DataView.module.scss';
+
+const DataView = ({
+  weatherData: {
+    name,
+    country,
+    temp,
+    temp_min,
+    temp_max,
+    feels_like,
+    humidity,
+    description,
+    icon,
+    speed,
+  },
+}) => {
+  return (
+    <>
+      <main className={styles.main}>
+        <DateTime />
+        <h1 className={styles.title}>
+          Weather in {name}, {country}
+        </h1>
+        <div className={styles.wrapper}>
+          <img
+            src={`http://openweathermap.org/img/w/${icon}.png`}
+            alt="Weather icon"
+            width="40"
+            height="40"
+            className={styles.icon}
+          />
+          <p className={styles.description}>{description}</p>
+        </div>
+      </main>
+      <div className={styles.wrapper}>
+        <img
+          src={temperature}
+          alt="Thermometer"
+          width="40"
+          height="40"
+          className={styles.icon}
+        />
+        <ul className={styles.description}>
+          <li>
+            <b>Feels like:</b> {feels_like} °С
+          </li>
+          <li>
+            <b>Current:</b> {temp} °С
+          </li>
+          <li>
+            <b>High:</b> {temp_max} °С
+          </li>
+          <li>
+            <b>Low:</b> {temp_min} °С
+          </li>
+        </ul>
+      </div>
+      <div className={styles.wrapper}>
+        <img
+          src={wind}
+          alt="Wind Speed"
+          width="40"
+          height="40"
+          className={styles.icon}
+        />
+        <p className={styles.description}>
+          <b>Wind Speed:</b> {speed} м/с
+        </p>
+      </div>
+      <div className={styles.wrapper}>
+        <img
+          src={himidity}
+          alt="Humidity"
+          width="40"
+          height="40"
+          className={styles.icon}
+        />
+        <p className={styles.description}>
+          <b>Humidity:</b> {humidity} %
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default DataView;
