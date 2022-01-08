a663d24f53619f0c042733e4baf8b6cfd736d2d9
Vadim-Vatrya <VatryaVV@gmail.com>
2022-01-09 01:54:12 +0200

    new styles

--- src/components/DataView/DataView.jsx
+++ src/components/DataView/DataView.jsx
@@ -2,7 +2,7 @@ import DateTime from '../DataTime';
 
 import temperature from '../../images/temperature.png';
 import wind from '../../images/wind.png';
-import himidity from '../../images/humidity.png';
+import humidityIcon from '../../images/humidity.png';
 
 import styles from './DataView.module.scss';
 
@@ -22,7 +22,7 @@ const DataView = ({
 }) => {
   return (
     <>
-      <main className={styles.main}>
+      <main className={styles.box}>
         <DateTime />
         <h1 className={styles.title}>
           Weather in {name}, {country}
@@ -31,60 +31,61 @@ const DataView = ({
           <img
             src={`http://openweathermap.org/img/w/${icon}.png`}
             alt="Weather icon"
-            width="40"
-            height="40"
+            // width="40"
+            // height="40"
             className={styles.icon}
           />
           <p className={styles.description}>{description}</p>
         </div>
+
+        <div className={styles.wrapper}>
+          <img
+            src={temperature}
+            alt="Thermometer"
+            // width="40"
+            // height="40"
+            className={styles.icon}
+          />
+          <ul className={styles.description}>
+            <li>
+              <b>Feels like:</b> {feels_like} °С
+            </li>
+            <li>
+              <b>Current:</b> {temp} °С
+            </li>
+            <li>
+              <b>High:</b> {temp_max} °С
+            </li>
+            <li>
+              <b>Low:</b> {temp_min} °С
+            </li>
+          </ul>
+        </div>
+        <div className={styles.wrapper}>
+          <img
+            src={wind}
+            alt="Wind Speed"
+            // width="40"
+            // height="40"
+            className={styles.icon}
+          />
+          <p className={styles.description}>
+            <b>Wind Speed:</b> {speed} м/с
+          </p>
+        </div>
+        <div className={styles.wrapper}>
+          <img
+            src={humidityIcon}
+            alt="Humidity"
+            // width="40"
+            // height="40"
+            className={styles.icon}
+          />
+          <p className={styles.description}>
+            <b>Humidity:</b> {humidity} %
+          </p>
+        </div>
       </main>
-      <div className={styles.wrapper}>
-        <img
-          src={temperature}
-          alt="Thermometer"
-          width="40"
-          height="40"
-          className={styles.icon}
-        />
-        <ul className={styles.description}>
-          <li>
-            <b>Feels like:</b> {feels_like} °С
-          </li>
-          <li>
-            <b>Current:</b> {temp} °С
-          </li>
-          <li>
-            <b>High:</b> {temp_max} °С
-          </li>
-          <li>
-            <b>Low:</b> {temp_min} °С
-          </li>
-        </ul>
-      </div>
-      <div className={styles.wrapper}>
-        <img
-          src={wind}
-          alt="Wind Speed"
-          width="40"
-          height="40"
-          className={styles.icon}
-        />
-        <p className={styles.description}>
-          <b>Wind Speed:</b> {speed} м/с
-        </p>
-      </div>
-      <div className={styles.wrapper}>
-        <img
-          src={himidity}
-          alt="Humidity"
-          width="40"
-          height="40"
-          className={styles.icon}
-        />
-        <p className={styles.description}>
-          <b>Humidity:</b> {humidity} %
-        </p>
-      </div>
     </>
   );
 };
